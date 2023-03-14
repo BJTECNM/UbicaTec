755efa427e8a431e37a3307d0006d971b1452417
BJTECNM <192t0220@itsm.edu.mx>
2023-03-14 15:11:08 -0600

    git commit

--- app/src/main/java/com/ubicatec/HomeActivity.kt
+++ app/src/main/java/com/ubicatec/HomeActivity.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.ubicatec.databinding.ActivityHomeBinding
+import org.jetbrains.annotations.NonNls
 
 enum class ProviderType {
     GOOGLE
@@ -29,6 +34,7 @@ class HomeActivity : AppCompatActivity() {
         prefs.putString("provider", provider)
         prefs.apply()
 
+        /*
         binding.btnLogOut.setOnClickListener {
             prefs.clear()
             prefs.apply()
@@ -37,6 +43,7 @@ class HomeActivity : AppCompatActivity() {
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
         }
+         */
 
         binding.btnRecordatorio.setOnClickListener {
             startActivity(Intent(applicationContext, RecordatorioActivity::class.java))
