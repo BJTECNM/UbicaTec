d51b70f24c29ee16e4a407ad8ae48d791311ad42
BJTECNM <192t0220@its.edu.mx>
2023-05-11 13:32:04 -0600

    -git commit

--- app/src/main/java/com/ubicatec/AulaActivity.kt
+++ app/src/main/java/com/ubicatec/AulaActivity.kt
@@ -3,8 +3,11 @@ package com.ubicatec
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.google.firebase.firestore.FirebaseFirestore
@@ -23,6 +26,7 @@ class AulaActivity : AppCompatActivity() {
         val lista = binding.nameAulas
         var arrayAdapter : ArrayAdapter<*>
         var nombreAulas = mutableListOf("")
+        var idAulas = mutableListOf("")
         val guardar = findViewById<ImageView>(R.id.btnSave)
         guardar.isVisible=false
 
@@ -32,6 +36,7 @@ class AulaActivity : AppCompatActivity() {
                 for (document in documents) {
                     Log.d(TAG, "${document.id} => ${document.data}")
                     nombreAulas.add(document.data["nombreAula"].toString())
+                    idAulas.add(document.id)
                 }
                 arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,nombreAulas)
                 lista.adapter = arrayAdapter
@@ -40,6 +45,16 @@ class AulaActivity : AppCompatActivity() {
                 Log.w(TAG, "Error getting documents: ", exception)
             }
 
+        lista.onItemClickListener = object : AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext, "ID ${idAulas[position]}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
