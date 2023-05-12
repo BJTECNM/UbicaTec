a00385370c1597ff80a0c6362092870aa2dbcb38
BJTECNM <192t0220@its.edu.mx>
2023-05-11 20:00:03 -0600

    -git commit

--- app/src/main/java/com/ubicatec/UbicAulaActivity.kt
+++ app/src/main/java/com/ubicatec/UbicAulaActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
 import com.ubicatec.databinding.ActivityUbicAulaBinding
 
@@ -22,14 +23,19 @@ class UbicAulaActivity : AppCompatActivity() {
         val idAula : String? = bundle?.getString("idAula")
         val guardar = findViewById<ImageView>(R.id.btnSave)
         guardar.isVisible=false
+        val imageView = binding.imgDescripcion
 
         if (idAula != null) {
             db.collection("aulas").document(idAula).get().addOnSuccessListener {
                     document ->
                 if (document != null) {
                     Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                    binding.titulo.text=(document.data!!["nombreAula"].toString())
-                    binding.llenadoInfo.text=("Descripción: "+document.data!!["descripcion"])
+                    binding.titulo.text = (document.data!!["nombreAula"].toString())
+                    binding.llenadoInfo.text = ("Descripción: ${document.data!!["descripcion"]}")
+                    var aux = document.data!!["imgDescripcion"].toString()
+                    Glide.with(applicationContext)
+                        .load(aux)
+                        .into(imageView)
                 } else {
                     Log.d(TAG, "No such document")
                 }
